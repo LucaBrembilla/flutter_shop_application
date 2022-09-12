e7d2484ea453676c652c6779fca0d379b7c2639d
LucaBrembilla <lucabremby00@gmail.com>
2022-09-13 00:13:26 +0200

    Update products.dart
    Updated the addProduct function to have a firebase api with a database.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -48,15 +51,30 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    final Uri url = Uri.parse(
+        "https://flutter-update-23ff1-default-rtdb.europe-west1.firebasedatabase.app/products.json");
+    http
+        .post(
+      url,
+      body: json.encode({
+        "title": product.title,
+        "description": product.description,
+        "price": product.price,
+        "imageUrl": product.imageUrl,
+        "isFavorite": product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
